7fd9748272b258e62dbc18bc92218c9a23ca3ac1
Carolina <carolina.villegas@alarcongroup.us>
2023-09-23 21:37:13 -0500

    Ad

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const router = new VueRouter({
   },
   routes: [
     { path: '/'
-    , redirect: { name: 'auth-login' } 
+    , redirect: { name: 'user-posts' } 
   },
     ...apps,
     ...dashboard,
