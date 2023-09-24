780f49fd0ea23b2bd74e2086f09da634b8aeeecf
Carolina <carolina.villegas@alarcongroup.us>
2023-09-23 23:07:30 -0500

    add:ssh

--- src/views/smart-content/service/axios.js
+++ src/views/smart-content/service/axios.js
@@ -3,20 +3,20 @@ import axios from 'axios'
 
 // eslint-disable-next-line import/prefer-default-export - generation
 export const smartContentApi = axios.create({
-  baseURL: 'http://20.106.131.22/generation/',
+  baseURL: 'https://20.106.131.22/generation/',
   // baseURL: 'https://neural-guard-366803.rj.r.appspot.com/api/v1',
   // params: {}
 })
 
 //- users
 export const smartContentApiUsers = axios.create({   
-  baseURL: 'http://20.106.131.22/users/',
+  baseURL: 'https://20.106.131.22/users/',
   // baseURL: 'https://neural-guard-366803.rj.r.appspot.com/api/v1',
   // params: {}
 })
 //- business
 export const smartContentApiClientsBusiness = axios.create({
-  baseURL: 'http://20.106.131.22/business/',
+  baseURL: 'https://20.106.131.22/business/',
   // baseURL: 'https://neural-guard-366803.rj.r.appspot.com/api/v1',
   // params: {}
 })
