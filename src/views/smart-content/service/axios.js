638573e85a6b605c2aa6997c11130f3486496e25
Carolina <carolina.villegas@alarcongroup.us>
2023-09-23 23:28:29 -0500

    ssl certificate

--- src/views/smart-content/service/axios.js
+++ src/views/smart-content/service/axios.js
@@ -3,20 +3,20 @@ import axios from 'axios'
 
 // eslint-disable-next-line import/prefer-default-export - generation
 export const smartContentApi = axios.create({
-  baseURL: 'https://20.106.131.22/generation/',
+  baseURL: 'https://smartcontent.eastus.cloudapp.azure.com/generation/',
   // baseURL: 'https://neural-guard-366803.rj.r.appspot.com/api/v1',
   // params: {}
 })
 
 //- users
 export const smartContentApiUsers = axios.create({   
-  baseURL: 'https://20.106.131.22/users/',
+  baseURL: 'https://smartcontent.eastus.cloudapp.azure.com/users/',
   // baseURL: 'https://neural-guard-366803.rj.r.appspot.com/api/v1',
   // params: {}
 })
 //- business
 export const smartContentApiClientsBusiness = axios.create({
-  baseURL: 'https://20.106.131.22/business/',
+  baseURL: 'https://smartcontent.eastus.cloudapp.azure.com/business/',
   // baseURL: 'https://neural-guard-366803.rj.r.appspot.com/api/v1',
   // params: {}
 })
